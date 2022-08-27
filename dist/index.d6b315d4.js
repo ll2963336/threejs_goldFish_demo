ca1506bfd1e2b55569ea9b17071b5adac8e806e1
Emojer J <37789416+ll2963336@users.noreply.github.com>
2022-08-27 09:45:40 +0800

    update

---
+++ dist/index.d6b315d4.js
@@ -0,0 +1,6 @@
+function randomIntFromInterval(min, max) {
+    // min and max included
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+//# sourceMappingURL=index.d6b315d4.js.map
